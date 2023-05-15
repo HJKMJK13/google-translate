6d26a6171ff6784db34ac3c938ed0c3e8f7f600e
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-05-15 19:15:46 +0500

    yangi javascript fayl ochib oldik

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-async function translate(text) {
-    let res = await axios.post(
-    `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`,
-    { q: text, target: "tr" }
-    );
-    let translation = res.data.data.translations[0].translatedText;
-    return translation;
-  }
+const selectTag=document.querySelector("select");
+selectTag.forEach(tag=>{
+    for(const key in object){
+        
+    }
+})
